0e1c4b9f43a7aaa5bb67323b5e2555dcfc39f46c
Lars Erik Grambo <0kvakk@gmail.com>
2025-03-18 14:24:35 +0100

    Start work on extention

--- rust-extention/godot_kva_hex_core/src/lib.rs
+++ rust-extention/godot_kva_hex_core/src/lib.rs
@@ -1,3 +1,24 @@
+use godot::prelude::*;
+
+#[derive(GodotClass)]
+#[class(base=RefCounted)]
+struct SpiralHexGrid {
+    data:Vec<HexContent>
+}
+
+impl SpiralHexGrid {
+    fn new() -> Self {
+        Self {  }
+    }
+}
+struct HexContent{
+
+}
+
+#[gdextension]
+unsafe impl ExtensionLibrary for SpiralHexGrid {}
+
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
