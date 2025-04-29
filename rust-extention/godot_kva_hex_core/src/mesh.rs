660f02652746f9047585f1bb0f6ebb40ad22391a
Lars Erik Grambo <0kvakk@gmail.com>
2025-04-29 14:37:46 +0200

    WIP prototype structure for animated geometry

--- rust-extention/godot_kva_hex_core/src/mesh.rs
+++ rust-extention/godot_kva_hex_core/src/mesh.rs
@@ -1,10 +1,60 @@
+use godot::classes::mesh::ArrayFormat;
 use godot::classes::{IMesh, Mesh};
 use godot::prelude::*;
+use bitflags::bitflags;
+use crate::SpiralHexGrid;
+
 #[derive(GodotClass)]
 #[class(base=Mesh)]
 struct SpiralHexMesh {
-
+    //if grid is not defined, layers will used without pulling this from grid.
+    //This will result in a mesh without heightdata, meaning it's flat
+    grid:Option<Gd<SpiralHexGrid>>,
+    //If a grid is not defined, layers can be set manually. A value of 0 will result in just one single tile.
+    layers:u8,
+    //flags are used to define what surface layers are rendered.
+    flags:RenderFlags,
+    //contains data regarding the current animation effect, or lack thereof.
+    animate_data:Option<AnimateData>,
+    //defines the orientation to render the tiles in, using "north" as Z-axis, has a flat surface towards north if true.
+    flat_north:bool
 }
+///stores data regarding an active animation. This includes a copy of data before animation, the current animation step, what tiles are being moved, origin and destination tiles of the animation, the geometry data of the moving geometry,
+/// and what else I can't think of while prototyping this structure right now :) .
+struct AnimateData {
+    step:AniStep
+}
+///Each step holds a time value, and an end value, messured in microseconds. These are used to lerp values for animation. Once t exceeds e, the animation is advanced to the next step. The excess value of t above e of the previus step is added to the next value t. This ensures consistant animation length.
+/// Lift: the tile(s) lifts from the grid. EndLift: Short pause. Move: the tiles move to their target destination. EndMove: short pause. Drop: Tiles drop down to their destination. EndDrop: short pause. Morph: tile height adjusts to the new geometry as tile data changes End: short pause. None  - No animation is running.
+enum AniStep{None, Lift{t:i32, e:i32}, EndLift{t:i32, e:i32}, Move{t:i32, e:i32}, EndMove{t:i32, e:i32}, Drop{t:i32, e:i32}, EndDrop{t:i32, e:i32}, Morph{t:i32, e:i32}, End{i:i32, e:i32}}
+
+bitflags! {
+    pub struct RenderFlags: u32 {
+        const READY  =      1 << 0;
+        const WALL_1 =      1 << 1;
+        const WALL_2 =      1 << 2;
+        const WALL_3 =      1 << 3;
+        const WALL_4 =      1 << 4;
+        const WALL_5 =      1 << 5;
+        const WALL_6 =      1 << 6;
+        const ANIMATING =   1 << 7;
+
+        // The source may set any bits
+        const _ = !0;
+    }
+}
+
+
+/* ideas
+Layer 0 is the grid itself
+Layer 1-6 are toggelable. They are the walls outside of the grid. Each layer is their own side of the grid.
+Layer 7 and 8 are reserved for animated geometry.
+layers 9 and 10 are reserved for walls for animated geometry
+layers 11 and 12 are reserved for walls covering "gaps" of animated geometry.
+layers 13 and 15 are reserved for ground convering undearneath animated geometry
+layers 15 and 16 are reserved for bedrock/floors covering "gaps" from aniamted geometry.
+*/
+
 #[godot_api]
 impl IMesh for SpiralHexMesh {
 	///Number of contigues surfaces in the mesh.
@@ -13,11 +63,19 @@ impl IMesh for SpiralHexMesh {
     }
 	///Number of vertecies per surface
     fn surface_get_array_len(&self, index: i32,) -> i32 {
-        if index == 0{
-			0 //TODO fetch number of vertecies 
-		} else {
-			0
-		}	
+        match index {
+            //TODO: get number of vertecies in base grid
+            0 => {if self.flags.contains(RenderFlags::READY) { 0 } else { 0 }}
+            1 => {if self.flags.contains(RenderFlags::WALL_1){ 0 } else { 0 }}
+            2 => {if self.flags.contains(RenderFlags::WALL_2){ 0 } else { 0 }}
+            3 => {if self.flags.contains(RenderFlags::WALL_3){ 0 } else { 0 }}
+            4 => {if self.flags.contains(RenderFlags::WALL_4){ 0 } else { 0 }}
+            5 => {if self.flags.contains(RenderFlags::WALL_5){ 0 } else { 0 }}
+            6 => {if self.flags.contains(RenderFlags::WALL_6){ 0 } else { 0 }}
+
+            _ => 0
+            
+        }	
     }
 
 	///Number of indecies per surface
@@ -47,15 +105,16 @@ impl IMesh for SpiralHexMesh {
 	///defines the format of the arrays, utalizing bitflags to set properties.
     fn surface_get_format(&self, _index: i32,) -> u32 {
 		//NOTE indexed arrays, using vertex colors instead of UVs.
-		Mesh::ARRAY_FORMAT_VERTEX | Mesh::ARRAY_FORMAT_INDEX | Mesh::ARRAY_FORMAT_COLOR
+		(ArrayFormat::VERTEX | ArrayFormat::INDEX | ArrayFormat::COLOR).ord() as u32
     }
 
     fn surface_get_primitive_type(&self, _index: i32,) -> u32 {
-        //NOTE triangle strips may be a thing to try.
+        if _index > 0 {0}
+        else {0}
 		
     }
 
-    fn surface_set_material(&mut self, _index: i32, material: Option< Gd< godot::classes::Material > >,) {
+    fn surface_set_material(&mut self, _index: i32, _material: Option< Gd< godot::classes::Material > >,) {
         //TODO: Store material
     }
 
@@ -73,7 +132,7 @@ impl IMesh for SpiralHexMesh {
 		StringName::from("nope")
     }
 
-    fn set_blend_shape_name(&mut self, _index: i32, name: StringName,) {
+    fn set_blend_shape_name(&mut self, _index: i32, _name: StringName,) {
         //NOTE: consider if blend shapes may be of any use to this mesh. doupt it.
 
     }
@@ -85,7 +144,7 @@ impl IMesh for SpiralHexMesh {
     
     fn init(base: godot::obj::Base < Self::Base >) -> Self {
         //TODO: Store base
-		Self {  }
+		Self {grid:None, layers:3u8, flags:RenderFlags::empty(), animate_data:None, flat_north:true}
     }
     
     fn to_string(&self) -> godot::builtin::GString {
@@ -94,7 +153,7 @@ impl IMesh for SpiralHexMesh {
 
     }
     
-    fn on_notification(&mut self, what: godot::classes::notify::ObjectNotification) {
+    fn on_notification(&mut self, _what: godot::classes::notify::ObjectNotification) {
         //do nothing for now.
     }
     
