029fda6ff2ad923f2127c9dd3a2beef9b62c4b98
Lars Erik Grambo <0kvakk@gmail.com>
2025-05-06 14:36:43 +0200

    WIP

--- rust-extention/godot_kva_hex_core/src/mesh.rs
+++ rust-extention/godot_kva_hex_core/src/mesh.rs
@@ -1,12 +1,17 @@
 use godot::classes::mesh::ArrayFormat;
 use godot::classes::{IMesh, Mesh};
+use godot::global::{PropertyHint, PropertyUsageFlags};
+use godot::meta::{ClassName, PropertyHintInfo};
 use godot::prelude::*;
 use bitflags::bitflags;
 use crate::SpiralHexGrid;
 
+const DEBUG_01:bool = false;
+
 #[derive(GodotClass)]
 #[class(base=Mesh)]
 struct SpiralHexMesh {
+    base:Base<Mesh>,
     grid_verticies:Vec<Vector3>,
     grid_indicies:Vec<i32>,
     grid_colors:Vec<Color>,
@@ -20,8 +25,23 @@ struct SpiralHexMesh {
     //contains data regarding the current animation effect, or lack thereof.
     animate_data:Option<AnimateData>,
     //defines the orientation to render the tiles in, using "north" as Z-axis, has a flat surface towards north if true.
-    flat_north:bool
+    flat_north:bool,
+    size:MeshSize,
+
 }
+
+enum MeshSize{
+    Scaled(Vector3),
+    Fixed(Vector3)
+}
+impl Default for MeshSize {
+    fn default() -> Self {
+        //height-values are in u8 integers between 0 and 255.
+        //They should be well scaled to fit the situation.
+        MeshSize::Fixed(Vector3{x:1.0, y:0.004, z:1.0})
+    }
+}
+
 ///stores data regarding an active animation. This includes a copy of data before animation, the current animation step, what tiles are being moved, origin and destination tiles of the animation, the geometry data of the moving geometry,
 /// and what else I can't think of while prototyping this structure right now :) .
 struct AnimateData {
@@ -89,15 +109,49 @@ impl SpiralHexMesh
         self.regenerate();
         return true;
     }
+    #[func]
+    fn get_num_tiles(&self) -> i32 {self._get_num_tiles() as i32}
+    fn _get_num_tiles(&self) -> usize {
+        3 * (self.layers as usize +1) * self.layers as usize + 1
+    }
 
     fn regenerate(&mut self){
-        let grid_vertex_size = 0usize;
-        let grid_index_size = 0usize;
+        use kva_hex_core::spiral;
+        const VERTS_PER_TILE:usize = 7; // six corners and a center makes 7 vertecies
+        const INDICIES_PER_TILE:usize = 6*3; //six triangles make one hexagon, there are 3 vertecies per triangle.
+        let grid_vertex_size = self._get_num_tiles() * VERTS_PER_TILE;
+        let grid_index_size = self._get_num_tiles()* INDICIES_PER_TILE;
 
         self.grid_verticies.resize(grid_vertex_size, Vector3::ZERO);
         self.grid_colors.resize(grid_vertex_size, Color::BLACK);
         self.grid_indicies.resize(grid_index_size, 0i32);
 
+        let scale = match self.size {
+            MeshSize::Scaled(s) => s,
+            MeshSize::Fixed(s) => {
+                let length = self.layers as f32 * 2f32+ 1f32;
+                //TODO: softcode to allow for alternate different types of height data input
+                Vector3{x:s.x / length, y:s.y / 255f32, z:s.z / length}
+            }
+        };
+
+        let heightdata: PackedByteArray = self.grid.as_ref().map_or(PackedByteArray::new(), |g|{Some(g.bind().get_heightdata().clone())});
+        for i in 0..self._get_num_tiles() {
+            let color:f64 = (i % 500) as f64 / 500f64;
+            let color = Color::from_hsv(color, 1.0, 1.0);
+            let hex = spiral::spiral_index_to_hex(i);
+            let height = heightdata.get(i).unwrap_or(0);
+
+            if DEBUG_01{
+                if i as i32 >= self.get_num_tiles() - 20 {
+                    godot_print!("draw - {}, {}, {}", hex.q, hex.r, hex.s());
+                }
+            }
+        }
+
+        //grid must be dropped so that self can be rused for base.
+        drop(grid);
+        self.base_mut().emit_changed();
     }
 }
 
@@ -199,8 +253,9 @@ impl IMesh for SpiralHexMesh {
     }
     
     fn init(base: godot::obj::Base < Self::Base >) -> Self {
+        
         //TODO: Store base
-		Self {grid:None, layers:3u8, flags:RenderFlags::empty(), animate_data:None, flat_north:true}
+		Self {base, grid:None, layers:3u8, flags:RenderFlags::empty(), animate_data:None, flat_north:true, grid_verticies:vec!(), grid_colors:vec!(), grid_indicies:vec!(), size: MeshSize::default()}
     }
     
     fn to_string(&self) -> godot::builtin::GString {
@@ -224,8 +279,12 @@ impl IMesh for SpiralHexMesh {
     }
     
     fn get_property_list(&mut self) -> Vec< godot::meta::PropertyInfo > {
-        //No properties to list for now.
-		vec![]
+        use godot::meta::PropertyInfo as Info;
+        vec![
+            Info{property_name:StringName::from("layers"), variant_type:VariantType::INT, class_name:ClassName::none(), usage: PropertyUsageFlags::DEFAULT,
+                hint_info:PropertyHintInfo{hint_string:GString::from("minimum 0, maximum 255. Restricted by unsigned 8 bit integer. This is the number of layers to render. Automatically set by grid(if defined), can be overwritten."), hint: PropertyHint::RANGE}}
+            ]
+        //TODO: list all properties that I may want to view or edit from inspector.
     }
     
     fn validate_property(&self, _property: &mut godot::meta::PropertyInfo) {
@@ -241,3 +300,20 @@ impl IMesh for SpiralHexMesh {
         //Nothing needs doing. Maybe. I donno...
     }
 }
+
+const FLAT_UP_CORNERS: [(f32, f32); 6] = [
+    (0.500, -0.866),     // 300°: (0.5, -√3/2) 
+    (1.000, 0.000),      // 0°: (1, 0)
+    (0.500, 0.866),      // 60°: (0.5, √3/2)
+    (-0.500, 0.866),     // 120°: (-0.5, √3/2)
+    (-1.000, 0.000),     // 180°: (-1, 0)
+    (-0.500, -0.866),    // 240°: (-0.5, -√3/2)
+];
+const POINTY_UP_CORNERS: [(f32, f32); 6] = [
+    (0.866, -0.500), 
+    (0.866, 0.500), 
+    (0.000, 1.000), 
+    (-0.866, 0.500), 
+    (-0.866, -0.500),  
+    (0.000, -1.000), 
+];
